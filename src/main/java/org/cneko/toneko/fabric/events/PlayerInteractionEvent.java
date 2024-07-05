5d93687a99041df665fb6fb7db1686219c49504e
CSneko <minecrafthetun@outlook.com>
2024-07-05 23:20:20 +0800

    添加一个客户端验证~

--- src/main/java/org/cneko/toneko/fabric/events/PlayerInteractionEvent.java
+++ src/main/java/org/cneko/toneko/fabric/events/PlayerInteractionEvent.java
@@ -1,9 +1,12 @@
 package org.cneko.toneko.fabric.events;
 
 import net.fabricmc.fabric.api.event.player.UseEntityCallback;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
 import net.minecraft.particle.ParticleTypes;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.EntityHitResult;
@@ -21,7 +24,7 @@ public class PlayerInteractionEvent {
 
     public static ActionResult useEntity(PlayerEntity player, World world, Hand hand, Entity entity, EntityHitResult hitResult) {
         // 目标实体为玩家
-        if(entity instanceof PlayerEntity nekoPlayer){
+        if(entity instanceof ServerPlayerEntity nekoPlayer && player instanceof ServerPlayerEntity sp){
             NekoQuery.Neko neko = NekoQuery.getNeko(nekoPlayer.getUuid());
             // 如果是猫娘且玩家是主人
             if(neko.isNeko() && neko.hasOwner(player.getUuid())){
@@ -29,15 +32,19 @@ public class PlayerInteractionEvent {
                 if(neko.hasQuirk(CARESS) && player.getMainHandStack().isEmpty()){
                     // 播放爱心粒子
                     nekoPlayer.getWorld().addParticle(ParticleTypes.HEART,nekoPlayer.getX()+1.8, nekoPlayer.getY(), nekoPlayer.getZ(),1,1,1);
+                    // 发送给客户端
+                    ParticleS2CPacket packet = new ParticleS2CPacket(ParticleTypes.HEART,true,player.getX()+1.8, player.getY(), player.getZ(),2,2,2,1,1);
+                    sp.networkHandler.sendPacket(packet);
+                    nekoPlayer.networkHandler.sendPacket(packet);
                     // 增加互动值
-                    neko.addXp(player.getUuid(), CARESS.getInteractionValue());
+                    //neko.addXp(player.getUuid(), CARESS.getInteractionValue());
                     // 发送消息文本
                     player.sendMessage(translatable("quirk.toneko.caress.use", TextUtil.getPlayerName(player)), true);
                     nekoPlayer.sendMessage(translatable("quirk.toneko.caress.be_used", TextUtil.getPlayerName(player)),true);
                     return ActionResult.SUCCESS;
                 }
             }
-            neko.save();
+            //neko.save();
         }
         return ActionResult.PASS;
     }
