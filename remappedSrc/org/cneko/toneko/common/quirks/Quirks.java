4e90d7678e267ebd0eebe2eb7628f70fb6d062a5
Justin Zhang <2691475984@qq.com>
2024-07-06 11:23:45 +0800

    clean

--- remappedSrc/org/cneko/toneko/common/quirks/Quirks.java
+++
@@ -1,9 +0,0 @@
-package org.cneko.toneko.common.quirks;
-
-public class Quirks {
-    public static CaressQuirk CARESS = new CaressQuirk("caress");
-
-    public static void init(){
-        QuirkRegister.register(CARESS);
-    }
-}
