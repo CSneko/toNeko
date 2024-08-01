5bde337174e94190f6c13ab74db09af908cdae36
crystalneko <minecrafthetun@outlook.com>
2024-08-01 20:56:15 +0800

    添加NekoCollector物品

--- src/main/java/org/cneko/toneko/fabric/client/items/NekoArmorTrinketsRenderer.java
+++ src/main/java/org/cneko/toneko/fabric/client/items/NekoArmorTrinketsRenderer.java
@@ -36,23 +36,26 @@ public class NekoArmorTrinketsRenderer {
         TrinketRendererRegistry.registerRenderer(NEKO_EARS,NEKO_EARS_TRINKET_RENDERER);
     }
 
+
     public static class NekoTailTrinketRenderer implements TrinketRenderer {
+        public NekoArmorRenderer.NekoTailRenderer renderer;
+
         @Override
         public void render(ItemStack item, SlotReference slotReference, EntityModel<? extends LivingEntity> model, MatrixStack poseStack, VertexConsumerProvider vertexConsumers, int light, LivingEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
             try {
-                NekoArmorRenderer.NekoTailRenderer render = NEKO_TAIL.getRenderer();
-                render.setItemStack(item);
-                render.setEntity(entity);
-                render.setBaseModel((BipedEntityModel<?>) model);
-                render.setSlot(EquipmentSlot.CHEST);
-                render.setAnimatable(NEKO_TAIL);
-
+                if (renderer == null){
+                    renderer = NEKO_TAIL.getRenderer();
+                    renderer.setItemStack(item);
+                    renderer.setEntity(entity);
+                    renderer.setBaseModel((BipedEntityModel<?>) model);
+                    renderer.setSlot(EquipmentSlot.CHEST);
+                    renderer.setAnimatable(NEKO_TAIL);
+                }
                 // 保存当前矩阵状态
                 poseStack.push();
 
                 // 执行渲染
-                render.render(poseStack, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(Registries.ITEM.getId(NEKO_EARS))), light, OverlayTexture.DEFAULT_UV, 1);
-
+                renderer.render(poseStack, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(Registries.ITEM.getId(NEKO_EARS))), light, OverlayTexture.DEFAULT_UV, 1);
             } catch (Exception ignored) {} finally {
                 // 恢复之前的矩阵状态
                 poseStack.pop();
@@ -62,21 +65,24 @@ public class NekoArmorTrinketsRenderer {
     }
 
     public static class NekoEarsTrinketRenderer implements TrinketRenderer {
+        public NekoArmorRenderer.NekoEarsRenderer renderer;
         @Override
         public void render(ItemStack item, SlotReference slotReference, EntityModel<? extends LivingEntity> model, MatrixStack poseStack, VertexConsumerProvider vertexConsumers, int light, LivingEntity entity, float limbAngle, float limbDistance, float tickDelta, float animationProgress, float headYaw, float headPitch) {
             try {
-                NekoArmorRenderer.NekoEarsRenderer render = NEKO_EARS.getRenderer();
-                render.setItemStack(item);
-                render.setEntity(entity);
-                render.setBaseModel((BipedEntityModel<?>) model);
-                render.setSlot(EquipmentSlot.HEAD);
-                render.setAnimatable(NEKO_EARS);
-
+                if (renderer == null){
+                    renderer = NEKO_EARS.getRenderer();
+                    renderer.setItemStack(item);
+                    renderer.setEntity(entity);
+                    renderer.setBaseModel((BipedEntityModel<?>) model);
+                    renderer.setSlot(EquipmentSlot.HEAD);
+                    renderer.setAnimatable(NEKO_EARS);
+                }
+                poseStack.push();
                 // 保存当前矩阵状态
                 poseStack.push();
 
                 // 执行渲染
-                render.render(poseStack, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(Registries.ITEM.getId(NEKO_EARS))), light, OverlayTexture.DEFAULT_UV, 1);
+                renderer.render(poseStack, vertexConsumers.getBuffer(RenderLayer.getEntityTranslucent(Registries.ITEM.getId(NEKO_EARS))), light, OverlayTexture.DEFAULT_UV, 1);
 
             } catch (Exception ignored) {} finally {
                 // 恢复之前的矩阵状态
