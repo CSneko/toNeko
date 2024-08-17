3513f4c07a4d52a2763907aae26cc9a0f49a70ee
crystalneko <minecrafthetun@outlook.com>
2024-08-17 16:59:14 +0800

    添加乘骑功能

--- build.sh
+++
@@ -1,9 +0,0 @@
-./gradlew runDatagen &      # 将命令放入后台运行
-pid=$!              # 获取该命令的PID
-sleep 60
-kill -9 $pid       # 60s后强制kill
-
-./gradlew build
-mkdir result
-cp -r fabric/build/libs/* result
-cp -r neoforge/build/libs/* result
